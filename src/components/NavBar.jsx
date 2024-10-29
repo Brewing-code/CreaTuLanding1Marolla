4eb15c952a50902f8da82f921e7ca682b0bbed27
Franco Marolla <franco@MacBook-Pro.local>
2024-10-28 22:42:15 -0300

    agrego # a los href

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,9 +9,9 @@ const NavBar = () => {
         <h1>Soldier Hops Cerveza</h1>
         <ul>
             <li><a href={"/"}>Home</a></li>
-            <li><a href="">Birras</a></li>
-            <li><a href="">Gorras</a></li>
-            <li><a href="">Remeras</a></li>
+            <li><a href="#">Birras</a></li>
+            <li><a href="#">Gorras</a></li>
+            <li><a href="#">Remeras</a></li>
         </ul>
         <CartWidget />
       </nav>
